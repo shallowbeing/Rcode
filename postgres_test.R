c4cd531e260b12a169ff8f0232c172340f05298f
shallowbeing <neil.lambert@gmail.com>
2025-04-09 07:59:40 +0100

    update

--- postgres_test.R
+++ postgres_test.R
@@ -2,17 +2,20 @@ library(RPostgreSQL)
 library(RPostgres)
 library(DBI)
 
-con<- dbConnect(RPostgres::Postgres(),
-                dbname="iris",
-                user="neil",
-                password="Hendrix01",
-                options="-c search_path=iris"
-                )
+con <- dbConnect(RPostgres::Postgres(),
+    dbname = "iris",
+    user = "neil",
+    password = "Hendrix01",
+    options = "-c search_path=iris"
+)
 
-q1<- ("SELECT * from iris")
-ti<- dbGetQuery(con, q1)
+q1 <- ("SELECT * from iris")
+ti <- dbGetQuery(con, q1)
 
-iris<-iris
-tableid<- Id(table="iris")
-dbWriteTable(con, tableid, iris, overwrite=T)
-  
+iris <- iris
+dbSendQuery(con, "delete from iris")
+tableid <- Id(table = "iris")
+dbWriteTable(con, tableid, iris, append = T)
+View(ti)
+plot1 <- plot(ti$Sepal.Length, ti$Sepal.Width)
+View(plot1)
